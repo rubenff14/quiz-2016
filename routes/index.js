847b9a46216a553a2ac0182382b3aa70756d44ed
Ruben Fernandez <rubenff_14@hotmail.com>
2016-10-24 11:04:47 +0200

    Primera Pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz 2016' });
 });
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
