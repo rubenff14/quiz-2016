097f4bb6265a9077423c567dc918847034f2d707
Ruben Fernandez <rubenff_14@hotmail.com>
2016-11-04 13:22:16 +0100

    Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,17 +1,26 @@
 var models = require('../models/models.js');
 
-exports.question = function (req, res) {
-	models.Quiz.findAll().then(function(quiz) {
-		res.render('quizes/question', { pregunta:quiz[0].pregunta})
+// GET /:quizes
+exports.index = function (req, res) {
+	models.Quiz.findAll().then(function(quizes) {
+		res.render('quizes/index.ejs', {quizes: quizes})
 	});
 };
 
+// GET /quizes/id
+exports.show = function (req, res) {
+	models.Quiz.findById(req.params.quizId).then(function(quiz) {
+		res.render('quizes/show.ejs', {quiz: quiz})
+	});
+};
+
+// GET /quizes/:id/answer
 exports.answer = function (req, res) {
-	models.Quiz.findAll().then(function(quiz) {
-		if(req.query.respuesta.toUpperCase() === quiz[0].respuesta.toUpperCase()) {
-			res.render('quizes/answer', {respuesta: 'Correcto'});
+	models.Quiz.findById(req.params.quizId).then(function(quiz) {
+		if(req.query.respuesta.toUpperCase() === quiz.respuesta.toUpperCase()) {
+			res.render('quizes/answer.ejs', {quiz: quiz, respuesta: 'Correcto'});
 		} else {
-			res.render('quizes/answer', {respuesta: 'Incorrecto'});
+			res.render('quizes/answer.ejs', {quiz: quiz, respuesta: 'Incorrecto'});
 		}	
 	});
 };
--- models/models.js
+++ models/models.js
@@ -21,6 +21,9 @@ sequelize.sync().then(function() {
 		if(count === 0) { // La tabla se inicializa solo si está vacía
 			Quiz.create({pregunta: 'Capital de Italia',
 						respuesta: 'Roma'
+			});
+			Quiz.create({pregunta: 'Capital de Portugal',
+						respuesta: 'Lisboa'
 			})
 		.then(function() {console.log('Base de datos inicializada')});
 		};
